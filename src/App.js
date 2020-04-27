4f52958ae2a74effb671fd792234f636f491df01
burnedEyes <jaroslaw.blazkow@gmail.com>
2020-04-28 00:09:59 +0200

    dodany formularz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: '',
+      password: '',
+      mail: '',
+      formErrors: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+      }
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            login:
+        <input type="text" name="login" onChange={e => this.setState({login: e.target.value})} />
+          </label>
+          <label>password:
+      <input type="password" name="password" onChange={e => this.setState({password: e.target.value})} />
+          </label>
+          <label>mail:
+      <input type="text" name="mail" onChange={e => this.setState({mail: e.target.value})}  />
+          </label>
+          <button>Send </button>
+        </form>
+      </div>
+
+    );
+  }
 }
 
 export default App;
