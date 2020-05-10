cc5f2d9672dc447881f27fd0d6616f713ad6b04a
burnedEyes <jaroslaw.blazkow@gmail.com>
2020-05-10 22:37:08 +0200

    Updates

---
+++ precache-manifest.756c8119bff4886009125c4217b5c227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d091d34d38de571c7125f14c8bdf47ff",
+    "url": "/tire/index.html"
+  },
+  {
+    "revision": "5e897a6ce26e4f6b9856",
+    "url": "/tire/static/css/main.24a4e262.chunk.css"
+  },
+  {
+    "revision": "ce9f49a8b5068d654f05",
+    "url": "/tire/static/js/2.032bb32c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tire/static/js/2.032bb32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e897a6ce26e4f6b9856",
+    "url": "/tire/static/js/main.4f79c05e.chunk.js"
+  },
+  {
+    "revision": "52cc0b36e526a4346b18",
+    "url": "/tire/static/js/runtime-main.1a62bb9c.js"
+  },
+  {
+    "revision": "c015e18ee7c30cc8366dceb9cbf9a70c",
+    "url": "/tire/static/media/bg.c015e18e.jpg"
+  }
+]);
